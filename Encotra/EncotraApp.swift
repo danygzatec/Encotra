924f065d6a8cefe4e704263e13151d58aa1e1b2a
danygzatec <A00829404@tec.mx>
2023-03-22 14:13:40 -0600

    Initial Commit

---
+++ Encotra/EncotraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncotraApp.swift
+//  Encotra
+//
+//  Created by Dany Garza on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EncotraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
